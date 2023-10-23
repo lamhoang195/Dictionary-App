882e8af37fced6002c395337ab9a2c23c521f6db
nmhung1294 <144262235+nmhung1294@users.noreply.github.com>
2023-10-23 15:56:44 +0700

    Update Word

---
+++ src/main/java/app/CommandLine/Game.java
@@ -0,0 +1,2 @@
+package app.CommandLine;public class Game {
+}
--- src/main/java/app/CommandLine/Word.java
+++ src/main/java/app/CommandLine/Word.java
@@ -1,31 +1,52 @@
 package app.CommandLine;
 
 public class Word {
-    private String wordTarget, wordExplain;
+    private String wordTarget;
+    private String wordExplain;
 
+    public Word() {
+
+    }
+
+    /**
+     * Init word.
+     *
+     * @param wordTarget       word in English
+     * @param wordExplain Explain - translate into Vietnamese
+     */
     public Word(String wordTarget, String wordExplain) {
         this.wordTarget = wordTarget.trim().toLowerCase();
         this.wordExplain = wordExplain.trim().toLowerCase();
     }
 
-    public Word() {
-        this("", "");
-    }
-
+    /**
+     * Get word.
+     *
+     * @return English word - meaning in Vietnamese
+     */
     public String getWordTarget() {
-        return wordTarget;
-    }
-
-    public void setWordTarget(String wordTarget) {
-        this.wordTarget = wordTarget;
+        return this.wordTarget;
     }
 
     public String getWordExplain() {
-        return wordExplain;
+        return this.wordExplain;
     }
+
+    /**
+     * Set word.
+     *
+     * @param wordTarget       English
+     */
+    public void setWord(String wordTarget) {
+        this.wordTarget = wordTarget;
+    }
+
+    /**
+     * Set word.
+     * @param wordExplain meaning.
+     */
 
     public void setWordExplain(String wordExplain) {
         this.wordExplain = wordExplain;
     }
 }
-
