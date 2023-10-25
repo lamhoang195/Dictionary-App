f6e78564021d8f98005bdab7f8d663b336955966
tbmk67uet <125368679+tbmk67uet@users.noreply.github.com>
2023-10-25 11:01:01 +0700

    update

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,10 +1,13 @@
 package app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class App extends Application {
     public static void main(String[] args) {
@@ -13,16 +16,19 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        System.out.println("Hiiii");
-        primaryStage.setTitle("Hellooooo");
-        Button button  = new Button();
-        button.setText("say Le Ba Hoang");
-        Button button1 = new Button();
-        button1.setText("He");
-        StackPane layout = new StackPane();
-        layout.getChildren().addAll(button, button1);
-        Scene scene = new Scene(layout, 300, 400);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+            URL resource = getClass().getResource("/GUI/DictionaryGui.fxml");
+            if (resource == null) {
+                System.err.println("Không thể tìm thấy tệp FXML.");
+                System.exit(1); // hoặc xử lý lỗi khác ở đây
+            }
+            primaryStage.setTitle("Dictionary");
+            Parent root = FXMLLoader.load(resource);
+            Scene scene = new Scene(root, 800, 600);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/app/Controller/SearchController.java
@@ -0,0 +1,18 @@
+package app.Controller;
+
+import app.CommandLine.Word;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class SearchController implements Initializable {
+    private final ArrayList<Word> searchWord = new ArrayList<>();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resources) {
+
+    }
+
+}
